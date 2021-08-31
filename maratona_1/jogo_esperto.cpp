ebe34531ae2ac2d8460edda8a2aa02070561286a
Eder Monteiro <eder.matheus.monteiro@gmail.com>
2021-08-31 10:41:04 -0300

    fix 1897 solution
    Signed-off-by: Eder Monteiro <eder.matheus.monteiro@gmail.com>

--- maratona_1/jogo_esperto.cpp
+++ maratona_1/jogo_esperto.cpp
@@ -1,65 +1,59 @@
 #include <iostream>
-#include <queue>
 #include <map>
+#include <queue>
 
-using namespace std;
-
-int calcula(int num, int op)
-{
-    int res;
-    if (op == 1)
-        res = num * 2;
-    else if (op == 2)
-        res = num * 3;
-    else if (op == 3)
-        res = num / 2;
-    else if (op == 4)
-        res = num / 3;
-    else if (op == 5)
-        res = num + 7;
-    else if (op == 6)
-        res = num - 7;
-    return res;
+int calcula(int num, int op) {
+  int res;
+  if (op == 1)
+    res = num * 2;
+  else if (op == 2)
+    res = num * 3;
+  else if (op == 3)
+    res = num / 2;
+  else if (op == 4)
+    res = num / 3;
+  else if (op == 5)
+    res = num + 7;
+  else if (op == 6)
+    res = num - 7;
+  return res;
 }
 
-int bfs(int origem, int destino)
-{
-    int v, res;
-    queue<int> fila;
-    map<int, int> custo;
+int bfs(int origem, int destino) {
+  int v, res;
+  std::queue<int> fila;
+  std::map<int, int> custo;
 
-    fila.push(origem);
-    custo[origem] = 1;
+  fila.push(origem);
+  custo[origem] = 1;
 
-    if (origem == destino)
-        return 1;
+  if (origem == destino)
+    return 1;
 
-    while (!fila.empty())
-    {
-        v = fila.front();
+  while (!fila.empty()) {
+    v = fila.front();
 
-        for (int op = 0; op < 7; op++)
-        {
-            res = calcula(v, op);
+    for (int op = 1; op < 7; op++) {
+      res = calcula(v, op);
 
-            if (custo[res] == 0)
-            {
-                custo[res] = custo[v] + 1;
-                fila.push(res);
-            }
-        }
-        if (custo[destino] > 0)
-            return custo[destino];
-
-        fila.pop();
+      if (custo[res] == 0) {
+        custo[res] = custo[v] + 1;
+        fila.push(res);
+      }
     }
-    return 0;
+    if (custo[destino] > 0)
+      return custo[destino];
+
+    fila.pop();
+  }
+  return 0;
 }
 
-int main()
-{
-    int n, m;
-    cin >> n >> m;
-    int saida = (bfs(n, m) - 1);
-    cout << saida;
+int main() {
+  int n, m;
+  std::cin >> n >> m;
+  int saida = (bfs(n, m) - 1);
+  std::cout << saida << "\n";
+
+  return 0;
 }
