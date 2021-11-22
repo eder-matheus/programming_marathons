68e447868a6f3a745373bbdd29e9bd17ace96e18
Eder Monteiro <eder.matheus.monteiro@gmail.com>
2021-11-22 10:03:20 -0300

    fix solution for uri-2063
    Signed-off-by: Eder Monteiro <eder.matheus.monteiro@gmail.com>

--- maratona_5/cacando.cpp
+++ maratona_5/cacando.cpp
@@ -2,37 +2,37 @@
 #include <iostream>
 #include <stdlib.h>
 
-using namespace std;
-
-long mdc(long n1, long n2) {
-	long r = n1 % n2;
-	while (r != 0) {
-		n1 = n2;
-		n2 = r;
-		r = n1 % n2;
-	}
-	return n2;
+long mdc(unsigned long n1, unsigned long n2) {
+  unsigned long r = n1 % n2;
+  while (r != 0) {
+    n1 = n2;
+    n2 = r;
+    r = n1 % n2;
+  }
+  return n2;
 }
 
 int main() {
-	long res = 1;
-	long t, buraco, num_buracos;
-    long buracos_vector[105];
+  unsigned long res = 1;
+  int buraco, num_buracos;
+  int buracos_vector[105];
 
-	cin >> num_buracos;
+  std::cin >> num_buracos;
 
-	for (long i = 1; i <= num_buracos; i++)
-		cin >> buracos_vector[i];
+  for (int i = 1; i <= num_buracos; i++)
+    std::cin >> buracos_vector[i];
     
-	for (long i = 1; i <= num_buracos; i++){
-		t = 1;
-		buraco = buracos_vector[i];
+  for (int i = 1; i <= num_buracos; i++){
+    int t = 1;
+    buraco = buracos_vector[i];
 
-		while (buraco != i) {
-			t++;
-			buraco = buracos_vector[buraco];
-		}
-		res = (t*res) / mdc(res, t);
-	}
-	cout << res << endl;
+    while (buraco != i) {
+      t++;
+      buraco = buracos_vector[buraco];
+    }
+    res = (res / mdc(res, t)) * t;
+  }
+  std::cout << res << "\n";
+
+  return 0;
 }
